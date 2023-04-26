d14cef9eb869c2ad281bb0f743130e9a289a87c1
CharmingDays <nawhpongmaji1@gmail.com>
2023-04-26 07:40:28 -0500

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,36 +1,6 @@
-interface RunesData {
-    [key:string]:RuneObj[]
-}
-interface RuneObj {
-    selectedPerkIds:number[];
-    primaryStyleId:number;
-    subStyleId:number;
-    name:string;
-    current:boolean;
-    rate?:number
-}
 
 interface Subscribers {
     [key:string]: Function[]
-}
-
-interface matchDataObj {
-    puuids:string[];
-    matchIds:string[];
-    collectedMatchIds: string[];
-    runes:RunesData;
-}
-
-interface _champions {
-    [name:string]:number
-}
-
-interface ChampionPopularityInterface {
-    matchIds:string[],
-    usedMatchIds:string[],
-    champions:_champions,
-    puuid:string,
-    bannedChampions:_champions
 }
 
 
@@ -48,28 +18,13 @@ interface LockfileData {
 }
 
 
-interface actionData {
-    actorCellId:number;
-    championId: number;
-    completed:boolean;
-    id: number;
-    isAllyAction:boolean;
-    isInProgress:boolean;
-    type: string
-}
+
 interface RequestOpts {
     url:string;
     method:string;
     body?:any
 }
 
-interface lcuSettings {
-    autoAccept:boolean;
-    autoBan:number[];
-    autoHover:number[];
-    autoPick:number[];
-    autoRunes:boolean;
-}
 
 
-export {lcuSettings,LockfileData,RunesData,RuneObj,matchDataObj,RequestOpts,Subscribers,ChampionPopularityInterface,actionData}
+export {LockfileData,RequestOpts,Subscribers}
